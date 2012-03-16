81236988d9dfce9bfd64f6f8dccd4d9ee36bcb67
Bruno Eberhard <bruno.eberhard@pop.ch>
2012-03-16 16:06:30 +0100

    Umbau von GeneratePersonAction zu ohne Swing

--- OpenEch/src/main/java/ch/openech/datagenerator/GeneratePersonAction.java
+++ OpenEch/src/main/java/ch/openech/datagenerator/GeneratePersonAction.java
@@ -1,46 +1,78 @@
 package ch.openech.datagenerator;
 
-import java.awt.event.ActionEvent;
+import java.util.List;
 
-import javax.swing.JOptionPane;
-
-import ch.openech.mj.resources.ResourceAction;
+import ch.openech.mj.db.model.Constants;
+import ch.openech.mj.db.model.annotation.Int;
+import ch.openech.mj.edit.Editor;
+import ch.openech.mj.edit.EditorDialogAction;
+import ch.openech.mj.edit.form.AbstractFormVisual;
+import ch.openech.mj.edit.form.FormVisual;
+import ch.openech.mj.edit.validation.ValidationMessage;
 import ch.openech.mj.swing.FrameManager;
+import ch.openech.mj.toolkit.ClientToolkit;
 import ch.openech.mj.util.StringUtils;
 import ch.openech.xml.write.EchNamespaceContext;
 
 
-public class GeneratePersonAction extends ResourceAction {
+public class GeneratePersonAction extends EditorDialogAction {
 
-	private EchNamespaceContext echNamespaceContext;
-	
 	public GeneratePersonAction(EchNamespaceContext echNamespaceContext) {
-		this.echNamespaceContext = echNamespaceContext;
+		super(new GeneratePersonEditor(echNamespaceContext));
+	}
+	
+	public static class GeneratePersonEditor extends Editor<GeneratePersonData> {
+
+		private final EchNamespaceContext echNamespaceContext;
+		
+		public GeneratePersonEditor(EchNamespaceContext echNamespaceContext) {
+			this.echNamespaceContext = echNamespaceContext;
+		}
+
+		@Override
+		protected FormVisual<GeneratePersonData> createForm() {
+			AbstractFormVisual<GeneratePersonData> form = new AbstractFormVisual<GeneratePersonData>(GeneratePersonData.class, null, true);
+			form.line(GeneratePersonData.GENERATE_PERSON_DATA.numberOfPersons);
+			return form;
+		}
+
+		@Override
+		protected GeneratePersonData load() {
+			return new GeneratePersonData();
+		}
+
+		@Override
+		protected void validate(GeneratePersonData object, List<ValidationMessage> resultList) {
+			// nothing to validate
+		}
+
+		@Override
+		protected boolean save(GeneratePersonData data) {
+			String input = data.numberOfPersons;
+			if (StringUtils.isBlank(input)) return true;
+			
+			try {
+				int count = Integer.parseInt(input);
+				if (count > 0) {
+					DataGenerator.generateData(echNamespaceContext, count, FrameManager.getInstance());
+				} else {
+					ClientToolkit.getToolkit().showError(null, input);
+					return false;
+				}
+			} catch (NumberFormatException x) {
+				ClientToolkit.getToolkit().showError(null, input);
+				return false;
+			}
+			return true;
+		}
+		
 	}
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		generate();
-	}
-	
-	public void generate() {
-		String input = (String)JOptionPane.showInputDialog(null, "Anzahl Personen: ",  "Eingabe", JOptionPane.INFORMATION_MESSAGE, null, null, 100);
-		if (StringUtils.isBlank(input)) return;
-		
-		try {
-			int count = Integer.parseInt(input);
-			if (count > 0) {
-				DataGenerator.generateData(echNamespaceContext, count, FrameManager.getInstance());
-			} else {
-				showErrorMessage(input);
-			}
-		} catch (NumberFormatException x) {
-			showErrorMessage(input);
-		}
-	}
-	
-	private void showErrorMessage(String input) {
-		JOptionPane.showMessageDialog(null, "Ungültige Eingabe: " + input, "Generierung nicht möglich", JOptionPane.ERROR_MESSAGE);
+	public static class GeneratePersonData {
+		public static final GeneratePersonData GENERATE_PERSON_DATA = Constants.of(GeneratePersonData.class);
+
+		@Int(4)
+		public String numberOfPersons;
 	}
 
 }
