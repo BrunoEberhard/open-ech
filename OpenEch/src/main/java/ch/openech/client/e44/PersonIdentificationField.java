c8b70d7f5e48a88a563bd9028ec6113bedc5da48
Bruno Eberhard <bruno.eberhard@pop.ch>
2012-04-15 12:45:48 +0200

    PersonIdentificationField gefixt

--- OpenEch/src/main/java/ch/openech/client/e44/PersonIdentificationField.java
+++ OpenEch/src/main/java/ch/openech/client/e44/PersonIdentificationField.java
@@ -5,11 +5,9 @@ import java.util.List;
 import ch.openech.client.ewk.SearchPersonPage;
 import ch.openech.dm.person.Person;
 import ch.openech.dm.person.PersonIdentification;
-import ch.openech.mj.edit.Editor;
 import ch.openech.mj.edit.SearchDialogAction;
 import ch.openech.mj.edit.fields.ObjectFlowField;
 import ch.openech.mj.edit.form.FormVisual;
-import ch.openech.mj.edit.validation.ValidationMessage;
 import ch.openech.server.EchServer;
 
 public class PersonIdentificationField extends ObjectFlowField<PersonIdentification> {
@@ -20,49 +18,23 @@ public class PersonIdentificationField extends ObjectFlowField<PersonIdentificat
 	
 	@Override
 	public FormVisual<PersonIdentification> createFormPanel() {
-		// not used
-		return null;
+		return new PersonIdentificationPanel();
 	}
 
 	@Override
 	protected void show(PersonIdentification object) {
 		addHtml(object.toHtml());
-		addAction(new RemoveObjectAction());
-		if (isEditable()) {
-	        addAction(new PersonSearchAction());
-	        // EditObjectAction kann nicht verwendet werden, sondern die PersonIdentificationEditAction,
-	        // weil der Typ vom FormPanel PersonIdentification und nicht Person ist
-	        addAction(new PersonIdentificationEditor());
+	}
+	
+	@Override
+	protected void showActions() {
+		if (isEmpty()) {
+			addAction(new RemoveObjectAction());
 		}
+        addAction(new PersonSearchAction());
+        addAction(new ObjectFieldEditor());
 	}
 
-//	public final class PersonSearchEditor extends Editor<Person> {
-//		
-//		@Override
-//		protected FormVisual<Person> createForm() {
-//			return new PersonSearchForm();
-//		}
-//
-//		@Override
-//		protected Person load() {
-//			// not used
-//			return null;
-//		}
-//
-//		@Override
-//		protected void validate(Person object, List<ValidationMessage> resultList) {
-//			// not used
-//		}
-//
-//		@Override
-//		protected boolean save(Person person) {
-//			if (person != null) {
-//				PersonIdentificationField.this.setObject(person.personIdentification);
-//			}
-//			return true;
-//		}
-//	}
-	
 	public final class PersonSearchAction extends SearchDialogAction<Person> {
 		
 		public PersonSearchAction() {
@@ -78,30 +50,6 @@ public class PersonIdentificationField extends ObjectFlowField<PersonIdentificat
 		@Override
 		protected void save(Person object) {
 			setObject(object.personIdentification);
-		}
-	}
-	
-	public class PersonIdentificationEditor extends Editor<PersonIdentification> {
-		
-		@Override
-		public FormVisual<PersonIdentification> createForm() {
-			return new PersonIdentificationPanel();
-		}
-
-		@Override
-		public PersonIdentification load() {
-			return PersonIdentificationField.this.getObject();
-		}
-
-		@Override
-		public boolean save(PersonIdentification personIdentification) {
-			PersonIdentificationField.this.setObject(personIdentification);
-			return true;
-		}
-
-		@Override
-		public void validate(PersonIdentification object, List<ValidationMessage> resultList) {
-			// nothing to validate
 		}
 	}
 	
