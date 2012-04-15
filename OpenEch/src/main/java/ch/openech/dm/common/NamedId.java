6158125a5d93cac3a88bdaae9fa0a8209e49dbd2
Bruno Eberhard <bruno.eberhard@pop.ch>
2012-04-15 08:20:26 +0200

    Namedid: OPEN_ECH_ID_CATEGORY (ohne Person)

--- OpenEch/src/main/java/ch/openech/dm/common/NamedId.java
+++ OpenEch/src/main/java/ch/openech/dm/common/NamedId.java
@@ -8,7 +8,7 @@ import ch.openech.mj.db.model.annotation.Varchar;
 public class NamedId {
 
 	public static final NamedId NAMED_ID = Constants.of(NamedId.class);
-	public static final String OPEN_ECH_PERSON_ID_CATEGORY = "OPENECH.LOC";
+	public static final String OPEN_ECH_ID_CATEGORY = "OPENECH.LOC";
 	
 	public String personIdCategory;
 	
@@ -24,11 +24,11 @@ public class NamedId {
 	}
 	
 	public boolean isOpenEch() {
-		return OPEN_ECH_PERSON_ID_CATEGORY.equals(personIdCategory);
+		return OPEN_ECH_ID_CATEGORY.equals(personIdCategory);
 	}
 	
 	public void setOpenEch() {
-		personIdCategory = OPEN_ECH_PERSON_ID_CATEGORY;
+		personIdCategory = OPEN_ECH_ID_CATEGORY;
 		personId = UUID.randomUUID().toString().replace("-", "").substring(0, 20);
 	}
 	
