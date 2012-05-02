8505cc4589b1aec3613e986a6fc2634350f1afee
Bruno Eberhard <bruno.eberhard@pop.ch>
2012-05-02 16:20:02 +0200

    OrganisationPanel Anzeige funktionierte nicht mehr

--- OpenEch/src/main/java/ch/openech/client/org/OrganisationPanel.java
+++ OpenEch/src/main/java/ch/openech/client/org/OrganisationPanel.java
@@ -60,9 +60,11 @@ public class OrganisationPanel extends EchFormPanel<Organisation> {
 		} else {
 			area(ORGANISATION.businessAddress, new HeadquarterField(ORGANISATION.headquarterOrganisation, editable));
 		}
-		setRequired(ORGANISATION.arrivalDate);
-		setRequired(ORGANISATION.reportingMunicipality);
-		setRequired(ORGANISATION.businessAddress);
+		if (editable) {
+			setRequired(ORGANISATION.arrivalDate);
+			setRequired(ORGANISATION.reportingMunicipality);
+			setRequired(ORGANISATION.businessAddress);
+		}
 	}
 
 	//
