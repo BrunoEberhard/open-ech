cb8f1d4e46852c4ea98ded404db791a47bd66671
bruno <bruno.eberhard@pop.ch>
2012-12-20 06:59:41 +0100

    StaxEch0008: Cleanup

--- OpenEch/src/main/java/ch/openech/xml/read/StaxEch0008.java
+++ OpenEch/src/main/java/ch/openech/xml/read/StaxEch0008.java
@@ -1,37 +1,25 @@
 package ch.openech.xml.read;
 
-import static ch.openech.xml.read.StaxEch.integer;
-import static ch.openech.xml.read.StaxEch.token;
-
 import javax.xml.stream.XMLEventReader;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.events.StartElement;
 import javax.xml.stream.events.XMLEvent;
 
 import ch.openech.dm.common.CountryIdentification;
-import ch.openech.mj.db.model.ColumnProperties;
-import ch.openech.mj.db.model.Constants;
 
 public class StaxEch0008 {
 
-//	public static CountryIdentification country(XMLEventReader xml) throws XMLStreamException {
-//		CountryIdentification countryIdentification = new CountryIdentification();
-//		country(xml, countryIdentification);
-//		return countryIdentification;
-//	}
-	
+	/*
+	 * Die Landinformation besteht nur aus drei einfachen Werten,
+	 * die über das generische simpleValue eingelesen werden können. 
+	 */
 	public static void country(XMLEventReader xml, CountryIdentification countryIdentification) throws XMLStreamException {
 		while (true) {
 			XMLEvent event = xml.nextEvent();
 			if (event.isStartElement()) {
 				StartElement startElement = event.asStartElement();
 				String startName = startElement.getName().getLocalPart();
-				
-				if (Constants.getProperty(CountryIdentification.COUNTRY_IDENTIFICATION.countryId).getFieldName().equals(startName)) {
-					ColumnProperties.setValue(countryIdentification, startName, integer(xml));
-				} else {
-					ColumnProperties.setValue(countryIdentification, startName, token(xml));
-				}
+				StaxEch.simpleValue(xml, countryIdentification, startName);
 			} else if (event.isEndElement()) return;
 			// else skip
 		}
