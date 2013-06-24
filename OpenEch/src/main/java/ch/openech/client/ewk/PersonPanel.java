ab68a34547de234a76b9de0efd17974d914da9c2
bruno <bruno.eberhard@pop.ch>
2013-06-24 10:46:20 +0200

    PersonPanel: Eltern-Feld ist nun ein spezialisierte Version. Eingabe der Mutter setzt den offiziellen Name, falls der noch leer ist. Kommentare und Warnungen entfernt.

--- OpenEch/src/main/java/ch/openech/client/ewk/PersonPanel.java
+++ OpenEch/src/main/java/ch/openech/client/ewk/PersonPanel.java
@@ -2,17 +2,18 @@ package ch.openech.client.ewk;
 
 import static ch.openech.dm.person.Person.*;
 import static ch.openech.dm.person.PersonIdentification.*;
+import static ch.openech.dm.person.Relation.*;
 import ch.openech.client.e10.AddressField;
 import ch.openech.client.e11.PlaceOfOriginField;
 import ch.openech.client.e11.PlaceReadOnlyField;
 import ch.openech.client.e11.ResidenceField;
 import ch.openech.client.e21.NameOfParentsField;
-import ch.openech.client.e21.PartnerField;
 import ch.openech.client.e21.RelationField;
 import ch.openech.client.e44.TechnicalIdsField;
 import ch.openech.client.ewk.event.EchForm;
 import ch.openech.dm.person.Person;
 import ch.openech.dm.person.PersonEditMode;
+import ch.openech.dm.person.Relation;
 import ch.openech.dm.person.Residence;
 import ch.openech.dm.types.Sex;
 import ch.openech.dm.types.TypeOfResidence;
@@ -21,7 +22,11 @@ import ch.openech.mj.autofill.NameWithFrequency;
 import ch.openech.mj.edit.fields.AbstractJodaField;
 import ch.openech.mj.edit.fields.EnumEditField;
 import ch.openech.mj.edit.fields.FormField;
+import ch.openech.mj.edit.fields.ObjectFlowField;
 import ch.openech.mj.edit.form.Form;
+import ch.openech.mj.edit.form.IForm;
+import ch.openech.mj.model.Keys;
+import ch.openech.mj.util.StringUtils;
 import ch.openech.xml.write.EchSchema;
 
 public class PersonPanel extends EchForm<Person>  {
@@ -151,14 +156,29 @@ public class PersonPanel extends EchForm<Person>  {
 		line(PERSON.callName, PERSON.languageOfCorrespondance);
 		line(PERSON.nationality, PERSON.religion);
 		line(PERSON.placeOfBirth);
-		PartnerField mother = new PartnerField(PERSON.getMother(), echSchema);
-		PartnerField father = new PartnerField(PERSON.getFather(), echSchema);
-		// Das ist zur Zeit nicht mehr möglich, das Mutter - Feld müsste oberhalb des Namensfeld sein
-		// mother.setConnectedNameField((TextField) getField(PERSON_IDENTIFICATION.officialName));
+		ParentField mother = new ParentField(PERSON.getMother());
+		ParentField father = new ParentField(PERSON.getFather());
 		area(mother, father);
 		area(new PlaceOfOriginField(PERSON.placeOfOrigin, false, editable), PERSON.foreign);
+		
+		addDependecy(PERSON.getMother(), new OfficialNameFromMotherUpdater(), PERSON.personIdentification.officialName);
 	}
 
+	private class OfficialNameFromMotherUpdater implements Form.PropertyUpdater<Relation, String, Person> {
+		@Override
+		public String update(Relation input, Person person) {
+			String actualValue = person.personIdentification.officialName;
+			if (input != null && StringUtils.isEmpty(actualValue)) {
+				if (input.partner != null) {
+					return input.partner.officialName;
+				} else if (input.address != null) {
+					return input.address.lastName;
+				}
+			}
+			return actualValue;
+		}
+	}
+	
 	// getter / setter
 
 	@Override
@@ -171,13 +191,13 @@ public class PersonPanel extends EchForm<Person>  {
 
 	private void disableDeathFieldIfAlive(Person person) {
 		if (mode == PersonEditMode.DISPLAY) {
-			((AbstractJodaField) getField(PERSON.dateOfDeath)).setEnabled(person.dateOfDeath != null);
+			((AbstractJodaField<?>) getField(PERSON.dateOfDeath)).setEnabled(person.dateOfDeath != null);
 		}
 	}
 
 	private void disableMoveOutFields(Person person) {
 		if (mode == PersonEditMode.DISPLAY) {
-			((AbstractJodaField) getField(PERSON.departureDate)).setEnabled(person.departureDate != null);
+			((AbstractJodaField<?>) getField(PERSON.departureDate)).setEnabled(person.departureDate != null);
 			((PlaceReadOnlyField) getField(PERSON.goesTo)).setEnabled(person.goesTo != null && !person.goesTo.isUnknown());
 			((AddressField) getField(PERSON.goesToAddress)).setEnabled(person.goesTo != null && person.goesTo.mailAddress != null && !person.goesTo.mailAddress.isEmpty());
 		}
@@ -198,21 +218,34 @@ public class PersonPanel extends EchForm<Person>  {
 		((ResidenceField) getField(PERSON.residence)).fillWithDemoData(typeOfResidence);
 	}
 	
-//	public static void main(String... args) {
-//		FrameManager.setSystemLookAndFeel();
-//		Resources.addResourceBundle(ResourceBundle.getBundle("ch.openech.resources.OpenEch"));
-//		ClientToolkit.setToolkit(new SwingClientToolkit());
-//
-//		PersonPanel personPanel = new PersonPanel(PersonEditMode.DISPLAY, null);
-//		personPanel.setObject(new Person());
-//		
-//		JFrame frame = new JFrame("Test");
-//		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//		frame.setLayout(new BorderLayout());
-//		frame.add((Component) personPanel.getComponent(), BorderLayout.CENTER);
-//		frame.pack();
-//		frame.setLocationRelativeTo(null);
-//		frame.setVisible(true);
-//	}
+
+	private class ParentField extends ObjectFlowField<Relation> {
+		
+		public ParentField(Relation key) {
+			super(Keys.getProperty(key));
+		}
+
+		public class PartnerEditor extends ObjectFieldEditor {
+			// Benötigt, damit dir richige Resource verwendet wird
+		}
+		
+		@Override
+		protected void show(Relation relation) {
+			addHtml(relation.identificationToHtml());
+		}
+
+		@Override
+		protected void showActions() {
+			addAction(new PartnerEditor());
+		}
+		
+		@Override
+		public IForm<Relation> createFormPanel() {
+			EchForm<Relation> form = new EchForm<>();
+			form.area(RELATION.partner);
+			form.area(new AddressField(RELATION.address, true));
+			return form;
+		}
+	}
 
 }
