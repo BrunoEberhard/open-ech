616dbb7b9e496e1366662033733c13e854fc4db2
bruno <bruno.eberhard@pop.ch>
2014-06-15 16:18:23 +0200

    PreferenceForm: Import changed

--- OpenEch/src/main/java/ch/openech/frontend/preferences/PreferenceForm.java
+++ OpenEch/src/main/java/ch/openech/frontend/preferences/PreferenceForm.java
@@ -1,5 +1,6 @@
 package ch.openech.frontend.preferences;
 
+import static ch.openech.frontend.preferences.OpenEchPreferences.*;
 import ch.openech.frontend.e07.MunicipalityField;
 import ch.openech.frontend.ewk.event.EchForm;
 
@@ -9,13 +10,13 @@ public class PreferenceForm extends EchForm<OpenEchPreferences> {
 		super(2);
 		
 		addTitle("Schema");
-		line(OpenEchPreferences.OPEN_ECH_PREFERENCES.applicationSchemaData.schema20);
-		line(OpenEchPreferences.OPEN_ECH_PREFERENCES.applicationSchemaData.schema93,OpenEchPreferences.OPEN_ECH_PREFERENCES.applicationSchemaData.schema148);
+		line(OPEN_ECH_PREFERENCES.applicationSchemaData.schema20);
+		line(OPEN_ECH_PREFERENCES.applicationSchemaData.schema93,OPEN_ECH_PREFERENCES.applicationSchemaData.schema148);
 
 		addTitle("Voreingestellte Werte");
-		line(new MunicipalityField(OpenEchPreferences.OPEN_ECH_PREFERENCES.preferencesDefaultsData.residence, false), OpenEchPreferences.OPEN_ECH_PREFERENCES.preferencesDefaultsData.plz);
-		line(OpenEchPreferences.OPEN_ECH_PREFERENCES.preferencesDefaultsData.cantonAbbreviation.canton);
-		line(OpenEchPreferences.OPEN_ECH_PREFERENCES.preferencesDefaultsData.language, OpenEchPreferences.OPEN_ECH_PREFERENCES.preferencesDefaultsData.religion);
+		line(new MunicipalityField(OPEN_ECH_PREFERENCES.preferencesDefaultsData.residence, false), OPEN_ECH_PREFERENCES.preferencesDefaultsData.plz);
+		line(OPEN_ECH_PREFERENCES.preferencesDefaultsData.cantonAbbreviation.canton);
+		line(OPEN_ECH_PREFERENCES.preferencesDefaultsData.language, OPEN_ECH_PREFERENCES.preferencesDefaultsData.religion);
 
 		text("<b>Hinweis:</b> Die Einstellungen werden erst bei neu geöffneten Ansichten aktiv");
 	}
