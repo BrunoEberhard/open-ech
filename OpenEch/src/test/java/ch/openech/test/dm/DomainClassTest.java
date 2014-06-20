3b905c1dbb229c23289621f4f8f151723362d478
bruno <bruno.eberhard@pop.ch>
2014-06-20 07:32:37 +0200

    Die Codes werden unterdessen direkt in den Domain Klassen hinzugefügt. Ist daher hier nicht mehr nötig.

--- OpenEch/src/test/java/ch/openech/test/dm/DomainClassTest.java
+++ OpenEch/src/test/java/ch/openech/test/dm/DomainClassTest.java
@@ -1,11 +1,8 @@
 package ch.openech.test.dm;
 
-import java.util.ResourceBundle;
-
 import junit.framework.Assert;
 
 import org.junit.Test;
-import org.minimalj.model.Codes;
 import org.minimalj.model.test.ModelTest;
 
 import ch.openech.model.organisation.Organisation;
@@ -15,15 +12,6 @@ public class DomainClassTest {
 
 	@Test
 	public void testOpenEchDomainClasses() {
-		boolean orgCodesAvailable = Codes.getCode("commercialRegisterStatus") != null;
-		if (!orgCodesAvailable) {
-			Codes.addCodes(ResourceBundle.getBundle("ch.openech.model.organisation.types.ech_organisation"));
-		}
-		boolean personCodesAvailable = Codes.getCode("basedOnLaw") != null;
-		if (!personCodesAvailable) {
-			Codes.addCodes(ResourceBundle.getBundle("ch.openech.model.person.types.ech_person"));
-		}
-		
 		ModelTest test = new ModelTest(Person.class, Organisation.class); // ThirdPartyMove.class
 		if (!test.getProblems().isEmpty()) {
 			for (String s : test.getProblems()) {
@@ -32,4 +20,5 @@ public class DomainClassTest {
 			Assert.fail();
 		}
 	}
+	
 }
