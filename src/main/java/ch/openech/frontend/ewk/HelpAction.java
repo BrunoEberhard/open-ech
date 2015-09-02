31de16d6a6c95f7af05da4841e0b307fa2584e86
bruno <bruno.eberhard@pop.ch>
2015-09-02 10:08:21 +0200

    Letzte Swing Abhängigkeit auskommentiert

--- src/main/java/ch/openech/frontend/ewk/HelpAction.java
+++ src/main/java/ch/openech/frontend/ewk/HelpAction.java
@@ -1,38 +1,28 @@
 package ch.openech.frontend.ewk;
 
-import java.awt.event.ActionEvent;
-import java.io.IOException;
-
-import javax.swing.JEditorPane;
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-import javax.swing.JScrollPane;
-
-import org.minimalj.frontend.swing.SwingResourceAction;
+import org.minimalj.frontend.action.Action;
 
 
-public class HelpAction extends SwingResourceAction {
-	private static final long serialVersionUID = 1L;
+public class HelpAction extends Action {
 	
-	private static JFrame helpFrame;
-
-	public void actionPerformed(ActionEvent actionEvent) {
-		try {
-			if (helpFrame == null) {
-				JEditorPane pane = new JEditorPane();
-				pane.setEditable(false);
-				pane.setContentType("text/html");
-				pane.setPage(HelpAction.class.getResource("resources/anleitung.html"));
-				helpFrame = new JFrame("Hilfe");
-				helpFrame.setSize(500, 600);
-				helpFrame.setLocationRelativeTo(null);
-				helpFrame.setContentPane(new JScrollPane(pane));
-			}
-			helpFrame.setVisible(true);
-		} catch (IOException e) {
-			JOptionPane.showMessageDialog(null, "Hilfe kann nicht angezeigt werden.");
-			e.printStackTrace();
-		}
+	@Override
+	public void action() {
+//		try {
+//			if (helpFrame == null) {
+//				JEditorPane pane = new JEditorPane();
+//				pane.setEditable(false);
+//				pane.setContentType("text/html");
+//				pane.setPage(HelpAction.class.getResource("resources/anleitung.html"));
+//				helpFrame = new JFrame("Hilfe");
+//				helpFrame.setSize(500, 600);
+//				helpFrame.setLocationRelativeTo(null);
+//				helpFrame.setContentPane(new JScrollPane(pane));
+//			}
+//			helpFrame.setVisible(true);
+//		} catch (IOException e) {
+//			JOptionPane.showMessageDialog(null, "Hilfe kann nicht angezeigt werden.");
+//			e.printStackTrace();
+//		}
 	}
 
 }
