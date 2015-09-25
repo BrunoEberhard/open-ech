7a664542b6dc6137a661418f5808d7b8d58fc712
bruno <bruno.eberhard@pop.ch>
2015-09-25 08:28:05 +0200

    TablePageWithDetail: possible to have TablePage as DetailPage
    Nachzug aus Framwork

--- src/main/java/ch/openech/frontend/page/PersonSearchPage.java
+++ src/main/java/ch/openech/frontend/page/PersonSearchPage.java
@@ -6,8 +6,6 @@ import java.util.List;
 
 import org.minimalj.application.Preferences;
 import org.minimalj.backend.Backend;
-import org.minimalj.frontend.Frontend;
-import org.minimalj.frontend.page.ObjectPage;
 import org.minimalj.frontend.page.SearchPage;
 import org.minimalj.transaction.criteria.Criteria;
 
@@ -18,7 +16,7 @@ import ch.openech.model.person.PersonSearch;
 import ch.openech.xml.write.EchSchema;
 
 
-public class PersonSearchPage extends SearchPage<PersonSearch, Person> {
+public class PersonSearchPage extends SearchPage<PersonSearch, PersonPage> {
 
 	public static final Object[] FIELDS = {
 		$.firstName, //
@@ -39,18 +37,25 @@ public class PersonSearchPage extends SearchPage<PersonSearch, Person> {
 		return Backend.persistence().read(PersonSearch.class, Criteria.search(query), 100);
 	}
 
-	@Override
-	protected Person load(PersonSearch personSearch) {
+	private Person load(PersonSearch personSearch) {
 		return Backend.persistence().read(Person.class, personSearch.id);
 	}
 
 	@Override
-	public ObjectPage<Person> createDetailPage(Person person) {
+	public PersonPage createDetailPage(PersonSearch personSearch) {
+		Person person = load(personSearch); 
 		OpenEchPreferences preferences = Preferences.getPreferences(OpenEchPreferences.class);
 		EchSchema0020 schema0020 = preferences.applicationSchemaData.schema20;
 		String version = schema0020.getVersion() + "." + schema0020.getMinorVersion();
 		EchSchema schema = EchSchema.getNamespaceContext(20, version);
 
 		return new PersonPage(schema, person);
+	}
+
+	@Override
+	protected PersonPage updateDetailPage(PersonPage detailPage, PersonSearch personSearch) {
+		Person person = load(personSearch); 
+		detailPage.setObject(person);
+		return detailPage;
 	}
 }
