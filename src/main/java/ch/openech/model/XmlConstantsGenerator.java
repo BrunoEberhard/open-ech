64ab8393114380e64fe3cc3104fc38c0f73ecf8e
Bruno Eberhard <bruno.eberhard@pop.ch>
2017-12-23 15:16:20 +0100

    eCH 217 hinzugefügt

--- src/main/java/ch/openech/model/XmlConstantsGenerator.java
+++ src/main/java/ch/openech/model/XmlConstantsGenerator.java
@@ -93,7 +93,7 @@ public class XmlConstantsGenerator {
 		generator.read(112, "1.0");
 		generator.read(148, "1.0");
 		generator.read(196, "1.0");
-		generator.read(217, "1.0");
+		generator.read(211, "1.0");
 		generator.print();
 	}
 	
