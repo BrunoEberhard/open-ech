807dff7e6f042e7cc6f86362220214fa0f26b73f
Bruno Eberhard <bruno.eberhard@pop.ch>
2017-12-30 08:05:27 +0100

    locality / gemeindenPlz.csv einlesen

--- src/main/java/ch/openech/transaction/ImportSwissDataTransaction.java
+++ src/main/java/ch/openech/transaction/ImportSwissDataTransaction.java
@@ -1,14 +1,18 @@
 package ch.openech.transaction;
 
+import java.util.List;
+
 import org.minimalj.backend.Backend;
 import org.minimalj.model.EnumUtils;
 import org.minimalj.repository.query.By;
 import org.minimalj.transaction.Transaction;
+import org.minimalj.util.CsvReader;
 
 import ch.openech.model.code.FederalRegister;
 import ch.openech.model.common.Canton;
 import ch.openech.model.common.CountryIdentification;
 import ch.openech.model.common.MunicipalityIdentification;
+import ch.openech.model.estate.Locality;
 import ch.openech.xml.read.StaxEch0071;
 import ch.openech.xml.read.StaxEch0072;
 
@@ -45,7 +49,35 @@ public class ImportSwissDataTransaction implements Transaction<Void> {
 			municipalityIdentification.municipalityName = EnumUtils.getText(federalRegister);
 			Backend.insert(municipalityIdentification);
 		}
+		
+		CsvReader csvReader = new CsvReader(getClass().getResourceAsStream("/ch/openech/gemeindenPlz.csv"));
+		List<GemeindenPlz> gemeinden = csvReader.readValues(GemeindenPlz.class);
+		for (GemeindenPlz gemeinde : gemeinden) {
+			Locality locality = new Locality();
+			locality.name.language = "de";
+			locality.name.nameLong = gemeinde.PLZNAMK;
+			locality.id = gemeinde.PLZ4 * 100 + gemeinde.PLZZ;
+			if (Backend.read(Locality.class, locality.id) == null) {
+				Backend.insert(locality);
+			}
+		}
 		return null;
 	}
 	
+	// https://www.bfs.admin.ch/bfs/de/home/grundlagen/agvch/gwr-korrespondenztabelle.html
+	public static class GemeindenPlz {
+		public Integer PLZ4, PLZZ;
+		public String PLZNAMK;
+		// public String ANTEIL_GEMEINDE, KTKZ;
+		// public Integer GDENR;
+		// public String GDENAMK;
+	}
+
+	/*
+	public static void main(String[] args) {
+		CsvReader csvReader = new CsvReader(ImportSwissDataTransaction.class.getResourceAsStream("/ch/openech/gemeindenPlz.csv"));
+		List<GemeindenPlz> gemeinden = csvReader.readValues(GemeindenPlz.class);
+		System.out.println(gemeinden.get(24).PLZNAMK);
+	}
+	*/
 }
