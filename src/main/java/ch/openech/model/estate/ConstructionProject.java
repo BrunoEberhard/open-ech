bcdfae4e260f75f2c960bed9b3d592facf511c48
Bruno Eberhard <bruno.eberhard@pop.ch>
2018-01-01 13:53:39 +0100

    Some work on constructionProject writer

--- src/main/java/ch/openech/model/estate/ConstructionProject.java
+++ src/main/java/ch/openech/model/estate/ConstructionProject.java
@@ -4,20 +4,28 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.minimalj.model.Keys;
 import org.minimalj.model.View;
+import org.minimalj.model.annotation.NotEmpty;
 import org.minimalj.model.annotation.Size;
 import org.minimalj.model.annotation.Sizes;
+import org.minimalj.model.validation.Validation;
+import org.minimalj.model.validation.ValidationMessage;
+import org.minimalj.repository.sql.EmptyObjects;
 
 import ch.openech.model.EchFormats;
 import ch.openech.model.common.Canton;
 import ch.openech.model.common.CountryIdentification;
 import ch.openech.model.common.MunicipalityIdentification;
 import ch.openech.model.common.NamedId;
+import ch.openech.model.types.EchCode;
 
 @Sizes(EchFormats.class)
 public class ConstructionProject {
-
-	// public List<NamedId> localId = new ArrayList<>();
+	public static final ConstructionProject $ = Keys.of(ConstructionProject.class);
+	
+	@NotEmpty
+	public List<NamedId> localId = new ArrayList<>();
 	public String EPROID;
 	public String officialConstructionProjectFileNo, extensionOfOfficialConstructionProjectFileNo;
 	
@@ -27,8 +35,11 @@ public class ConstructionProject {
 	public Integer newBuildingsWithoutResidentialPurposeTotal;
 	public Integer newDwellingsCompleted;
 	public Integer newDwellingsTotal;
+	public TypeOfConstructionProject typeOfConstructionProject;
 
 	public final ConstructionLocalisation constructionLocalisation = new ConstructionLocalisation();
+	
+	public TypeOfPermit typeOfPermit;
 	
 	public LocalDate permitExpirationDate, permitIssueDate, announcementDate, declinationDate, startDate,
 			completionDate, suspensionDate, withdrawlDate;
@@ -38,17 +49,47 @@ public class ConstructionProject {
 	@Size(EchFormats.constructionProjectDescription)
 	public String description;
 	
-	public static class ConstructionLocalisation {
+	public static class ConstructionLocalisation implements Validation {
+		public static final ConstructionLocalisation $ = Keys.of(ConstructionLocalisation.class);
+		
+		// ein von den drei
 		public MunicipalityIdentification municipality;
 		public Canton canton;
 		public CountryIdentification country;
+		
+		@Override
+		public List<ValidationMessage> validate() {
+			if (EmptyObjects.isEmpty(municipality) && EmptyObjects.isEmpty(canton) && EmptyObjects.isEmpty(country)) {
+				return Validation.message(ConstructionLocalisation.$.municipality, "Gemeinde, Kanton oder Land muss eingegeben werden");
+			} else {
+				return null;
+			}
+		}
 	}
 	
 	public static class ConstructionProjectIdentification implements View<ConstructionProject> {
 
 		public List<NamedId> localId = new ArrayList<>();
-		public String EPROID;
+		public Integer EPROID;
 		public String officialConstructionProjectFileNo, extensionOfOfficialConstructionProjectFileNo;
 
+	}
+	
+	public static enum TypeOfConstructionProject implements EchCode {
+		_6010, _6011;
+
+		@Override
+		public String getValue() {
+			return name().substring(1);
+		}
+	}
+	
+	public static enum TypeOfPermit implements EchCode {
+		_5000, _5001, _5002, _5003, _5004, _5005, _5006, _5007, _5008, _5009, _5011, _5012, _5015, _5021, _5022, _5023, _5031, _5041, _5043, _5044, _5051, _5061, _5062, _5063, _5064, _5071;
+
+		@Override
+		public String getValue() {
+			return name().substring(1);
+		}
 	}
 }
--- src/main/java/ch/openech/xml/write/WriterEch0129.java
+++ src/main/java/ch/openech/xml/write/WriterEch0129.java
@@ -5,11 +5,14 @@ import static ch.openech.model.XmlConstants.REALESTATE;
 import java.util.List;
 
 import org.minimalj.model.ViewUtil;
+import org.minimalj.repository.sql.EmptyObjects;
 
 import ch.openech.model.XmlConstants;
 import ch.openech.model.common.NamedId;
 import ch.openech.model.common.NamedMetaData;
 import ch.openech.model.estate.Building;
+import ch.openech.model.estate.ConstructionProject;
+import ch.openech.model.estate.ConstructionProject.ConstructionLocalisation;
 import ch.openech.model.estate.Realestate;
 import ch.openech.model.estate.Realestate.RealestateIdentification;
 
@@ -17,8 +20,13 @@ public class WriterEch0129 {
 
 	public final String URI;
 	
+	public final WriterEch0007 ech7;
+	public final WriterEch0008 ech8;
+	
 	public WriterEch0129(EchSchema context) {
 		URI = context.getNamespaceURI(129);
+		ech7 = new WriterEch0007(context);
+		ech8 = new WriterEch0008(context);
 	}
 	
 	public void realestate(WriterElement parent, Realestate realestate) throws Exception {
@@ -75,6 +83,41 @@ public class WriterEch0129 {
 		default:
 			break;
 		}
+	}
+	
+	public void constructionProject(WriterElement parent, ConstructionProject project) throws Exception {
+		WriterElement writer = parent.create(URI, XmlConstants.CONSTRUCTION_PROJECT);
+		constructionProjectIdentification(writer, project);
+		writer.values(project, XmlConstants.NEW_BUILDINGS_FOR_RESIDENTIAL_PURPOSE_COMPLETED,
+				XmlConstants.NEW_BUILDINGS_FOR_RESIDENTIAL_PURPOSE_TOTAL,
+				XmlConstants.NEW_BUILDINGS_WITHOUT_RESIDENTIAL_PURPOSE_COMPLETED,
+				XmlConstants.NEW_BUILDINGS_WITHOUT_RESIDENTIAL_PURPOSE_TOTAL,
+				XmlConstants.NEW_DWELLINGS_COMPLETED,
+				XmlConstants.NEW_DWELLINGS_TOTAL,
+				XmlConstants.TYPE_OF_CONSTRUCTION_PROJECT);
+		constructionLocalisation(writer, project.constructionLocalisation);
+		writer.values(project, 
+				"permitExpirationDate", "permitIssueDate", "announcementDate", "declinationDate", "startDate",
+				"completionDate", "suspensionDate", "withdrawlDate");
+		// TODO rest
+		
+	}
+	
+	public void constructionProjectIdentification(WriterElement parent, ConstructionProject identification) throws Exception {
+		WriterElement writer = parent.create(URI, "constructionProjectIdentification");
+		namedId(writer, identification.localId, XmlConstants.LOCAL_I_D);
+		writer.values(identification, "EPROID", XmlConstants.OFFICIAL_CONSTRUCTION_PROJECT_FILE_NO, XmlConstants.EXTENSION_OF_OFFICIAL_CONSTRUCTION_PROJECT_FILE_NO);
+	}
+	
+	public void constructionLocalisation(WriterElement parent, ConstructionLocalisation constructionLocalisation) throws Exception {
+		WriterElement writer = parent.create(URI, XmlConstants.CONSTRUCTION_LOCALISATION);
+		if (!EmptyObjects.isEmpty(constructionLocalisation.municipality)) {
+			ech7.municipality(writer, XmlConstants.MUNICIPALITY, constructionLocalisation.municipality);
+		} else if (!EmptyObjects.isEmpty(constructionLocalisation.canton)) {
+			writer.text(XmlConstants.CANTON, constructionLocalisation.canton.id);
+		} else if (!EmptyObjects.isEmpty(constructionLocalisation.country)) {
+			ech8.country(writer, XmlConstants.COUNTRY, constructionLocalisation.country);
+		} 
 	}
 	
 	public void namedId(WriterElement parent, List<NamedId> namedIds, String name) throws Exception {
