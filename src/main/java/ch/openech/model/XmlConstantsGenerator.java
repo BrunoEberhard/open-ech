5f6f859b436e0439605ed462bc75229a5b76dea1
Bruno Eberhard <bruno.eberhard@pop.ch>
2018-01-02 08:40:02 +0100

    readd 217

--- src/main/java/ch/openech/model/XmlConstantsGenerator.java
+++ src/main/java/ch/openech/model/XmlConstantsGenerator.java
@@ -95,6 +95,7 @@ public class XmlConstantsGenerator {
 		generator.read(148, "1.0");
 		generator.read(196, "1.0");
 		generator.read(211, "1.0");
+		generator.read(217, "1.0");
 		generator.print();
 	}
 	
