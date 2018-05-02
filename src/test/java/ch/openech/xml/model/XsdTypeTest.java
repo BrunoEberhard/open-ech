bbad445b97dc3db079b5255c60519b1d83a8efe7
Bruno Eberhard <bruno.eberhard@pop.ch>
2018-05-02 19:29:55 +0200

    XsdTypeTest fix

--- src/test/java/ch/openech/xml/model/XsdTypeTest.java
+++ src/test/java/ch/openech/xml/model/XsdTypeTest.java
@@ -3,19 +3,19 @@ package ch.openech.xml.model;
 import org.junit.Assert;
 import org.junit.Test;
 
-import ch.openech.xml.XsdModel;
+import ch.openech.xml.EchSchemas;
 
 public class XsdTypeTest {
 
 	@Test
 	public void testPackageName1() {
 		String namespace = "http://www.ech.ch/xmlns/eCH-0135/1";
-		Assert.assertEquals("ch.ech.ech0135.v1", .packageName(namespace));
+		Assert.assertEquals("ch.ech.ech0135.v1", EchSchemas.packageName(namespace));
 	}
 
 	@Test
 	public void testPackageName2() {
 		String namespace = "http://www.ech.ch/xmlns/eCH-0147/T0/1";
-		Assert.assertEquals("ch.ech.ech0147.t0.v1", XsdSchema.packageName(namespace));
+		Assert.assertEquals("ch.ech.ech0147.t0.v1", EchSchemas.packageName(namespace));
 	}
 }
