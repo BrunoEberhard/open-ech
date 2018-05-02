4ec60b4226b96f67671cba30f8c042ea1f416dba
Bruno Eberhard <bruno.eberhard@pop.ch>
2018-05-02 06:36:22 +0200

    Funktioniert noch nicht perfekt

--- src/main/java/ch/openech/model/EchXmlDownload.java
+++ src/main/java/ch/openech/model/EchXmlDownload.java
@@ -7,6 +7,8 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
@@ -51,37 +53,67 @@ public class EchXmlDownload {
 	
 	
 	public boolean download(int rootNumber, int major, int minor) {
+		if (rootNumber == 147) {
+			return download147(major, minor);
+		}
+		
 		String rootNamespaceURI = EchNamespaceUtil.schemaURI(rootNumber, "" + major);
 		String rootNamespaceLocation = EchNamespaceUtil.schemaLocation(rootNamespaceURI, "" + minor);
 
 		if (locations.contains(rootNamespaceLocation)) {
-			return false;
+			return true;
 		}
 		locations.add(rootNamespaceLocation);
 		
-		try {
-			return download(rootNamespaceLocation);
-		} catch (Exception x) {
-			x.printStackTrace();
-			return false;
-		}
+		boolean versionExists = download(rootNamespaceLocation);
+		return versionExists;
 	}
 	
-	private boolean download(String namespaceLocation) throws XMLStreamException, IOException {
+	public boolean download147(int major, int minor) {
+		boolean versionExists = true;
+		for (int part = 0; part < 3; part++) {
+			String rootNamespaceLocation = "http://www.ech.ch/xmlns/eCH-0147/" + major + "." + minor + "/eCH-0147T" + part + ".xsd";
+			
+			if (locations.contains(rootNamespaceLocation)) {
+				continue;
+			}
+			locations.add(rootNamespaceLocation);
+			
+			versionExists = download(rootNamespaceLocation);
+		}
+		return versionExists;
+	}
+	
+	private boolean download(String namespaceLocation) {
+		System.out.println("Download: " + namespaceLocation);
 		String fileName = namespaceLocation.substring(namespaceLocation.lastIndexOf("/"));
 		File file = new File("src/main/xml/ch/ech/xmlns" + fileName);
 		if (!file.exists()) {
-			URL url = new URL(namespaceLocation);
-			try (ReadableByteChannel rbc = Channels.newChannel(url.openStream())) {
-				try (FileOutputStream fos = new FileOutputStream(file)) {
-					fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+			try {
+				URL url = new URL(namespaceLocation);
+				try (InputStream stream = url.openStream()) {
+					try (ReadableByteChannel rbc = Channels.newChannel(stream)) {
+						try (FileOutputStream fos = new FileOutputStream(file)) {
+							fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+						}
+					}
+				} catch (FileNotFoundException f) {
+					// Diese Version exsistiert nicht.
+					return false;
+				} catch (IOException e) {
+					throw new RuntimeException(e);
 				}
-			} catch (FileNotFoundException f) {
-				return false;
+			} catch (MalformedURLException e) {
+				throw new RuntimeException(e);
+			}
+
+			try {
+				process(file);
+			} catch (XMLStreamException | IOException e) {
+				throw new RuntimeException(e);
 			}
 		}
 		
-		process(file);
 		return true;
 	}
 	
@@ -133,7 +165,10 @@ public class EchXmlDownload {
 
 	public static void main(String... args) {
 		EchXmlDownload download = new EchXmlDownload();
-		download.download(211);
+		download.download(71);
+		download.download(72);
+//		download.download(147);
+//		download.download(211);
 //		download.download(21);
 //		download.download(78);
 //		download.download(93);
