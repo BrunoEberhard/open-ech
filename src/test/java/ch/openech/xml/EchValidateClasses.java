c8f585b5b98ecfbc66e161e41259a9f1dda8b2df
Bruno Eberhard <bruno.eberhard@pop.ch>
2018-06-21 10:09:21 +0200

    EchValidateClasses: more info

--- src/test/java/ch/openech/xml/EchValidateClasses.java
+++ src/test/java/ch/openech/xml/EchValidateClasses.java
@@ -3,6 +3,7 @@ package ch.openech.xml;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.minimalj.metamodel.generator.ClassValidator;
 import org.minimalj.metamodel.model.MjEntity;
@@ -12,10 +13,17 @@ public class EchValidateClasses {
 	@Test
 	public void validateEchClasses() {
 		ClassValidator validator = new ClassValidator();
+		boolean valid = true;
 		for (XsdModel model : EchSchemas.getXsdModels()) {
 			Collection<MjEntity> entities = model.getEntities();
 			entities = entities.stream().filter(EchSchemas::filter).collect(Collectors.toList());
-			validator.validate(entities);
+			try {
+				validator.validate(entities);
+			} catch (RuntimeException x) {
+				valid = false;
+				System.out.println(model.getNamespace() + ": " + x.getMessage());
+			}
 		}
+		Assert.assertTrue(valid);
 	}
 }
