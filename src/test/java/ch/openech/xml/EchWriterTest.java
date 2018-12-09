bb4829e1047ec52f92ee7935b4918a0bf08877ee
Bruno Eberhard <bruno.eberhard@pop.ch>
2018-12-09 18:12:44 +0100

    EchWriterTest: Rountrip read / write ech 71

--- src/test/java/ch/openech/xml/EchWriterTest.java
+++ src/test/java/ch/openech/xml/EchWriterTest.java
@@ -4,15 +4,20 @@ import java.io.StringWriter;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
+import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import ch.ech.ech0071.Canton;
 import ch.ech.ech0071.CantonAbbreviation;
+import ch.ech.ech0071.District;
 import ch.ech.ech0071.Nomenclature;
+import ch.openech.model.EchSchemaValidation;
 
 public class EchWriterTest {
 
 	@Test
+	@Ignore
 	public void testEch71WriteCantons() throws Exception {
 		Nomenclature nomenclature = new Nomenclature();
 		nomenclature.validFrom = LocalDate.now().minusMonths(1);
@@ -27,13 +32,40 @@ public class EchWriterTest {
 			nomenclature.cantons.canton.add(canton);
 		}
 
+		nomenclature.districts.district = new ArrayList<>();
+		District district = new District();
+		district.id = 12345;
+		district.cantonId = 1;
+		district.districtLongName = "See Gaster";
+		nomenclature.districts.district.add(district);
+
 		StringWriter stringWriter = new StringWriter();
 		EchWriter writer = new EchWriter(stringWriter);
 		writer.writeDocument(nomenclature);
 		writer.close();
 
-		System.out.println(stringWriter.toString());
+		String string = stringWriter.toString();
+		System.out.println(string);
 
+		System.out.println(EchSchemaValidation.validate(string));
+
+	}
+
+	@Test
+	public void testReadWriteEch071() throws Exception {
+		EchReader reader = new EchReader(getClass().getClassLoader().getResourceAsStream("eCH0071.xml"));
+		Nomenclature nomenclature = (Nomenclature) reader.read();
+		reader.close();
+
+		StringWriter stringWriter = new StringWriter();
+		EchWriter writer = new EchWriter(stringWriter);
+		writer.writeDocument(nomenclature);
+		writer.close();
+
+		String string = stringWriter.toString();
+		System.out.println(string.substring(0, 1000));
+
+		Assert.assertEquals(EchSchemaValidation.OK, EchSchemaValidation.validate(string));
 	}
 
 }
