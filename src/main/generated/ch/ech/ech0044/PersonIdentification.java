2d40e4492f6e691c953ca9b715154ca729df4c73
Bruno Eberhard <bruno.eberhard@pop.ch>
2019-04-25 09:44:45 +0200

    PersonIdentification implements Rendering

--- src/main/generated/ch/ech/ech0044/PersonIdentification.java
+++ src/main/generated/ch/ech/ech0044/PersonIdentification.java
@@ -3,8 +3,11 @@ package ch.ech.ech0044;
 import java.util.List;
 
 import org.minimalj.model.Keys;
+import org.minimalj.model.Rendering;
 import org.minimalj.model.annotation.NotEmpty;
 import org.minimalj.model.annotation.Size;
+import org.minimalj.repository.sql.EmptyObjects;
+import org.minimalj.util.StringUtils;
 
 // handmade : die NotEmpty auf officialName, firstName, sex entfernt, damit die Entity auch als "Light" Version
 // verwendet werden kann.
@@ -14,7 +17,7 @@ import org.minimalj.model.annotation.Size;
 // - bis Version 5 von ech0011
 // - bis Version 4 von ech0021
 
-public class PersonIdentification {
+public class PersonIdentification implements Rendering {
 	public static final PersonIdentification $ = Keys.of(PersonIdentification.class);
 
 	public Object id;
@@ -33,4 +36,32 @@ public class PersonIdentification {
 	// bei "Light" nicht zwingend, sonst schon
 	public Sex sex;
 	public final ch.openech.xml.DatePartiallyKnown dateOfBirth = new ch.openech.xml.DatePartiallyKnown();
+
+	public boolean isLight() {
+		return EmptyObjects.isEmpty(localPersonId) || sex == null || EmptyObjects.isEmpty(dateOfBirth);
+	}
+
+	@Override
+	public CharSequence render() {
+		StringBuilder s = new StringBuilder();
+		boolean empty = true;
+		if (!StringUtils.isEmpty(firstName)) {
+			s.append(firstName);
+			empty = false;
+		}
+		if (!StringUtils.isEmpty(officialName)) {
+			if (!empty) {
+				s.append(' ');
+			}
+			s.append(officialName);
+			empty = false;
+		}
+		if (!EmptyObjects.isEmpty(dateOfBirth)) {
+			if (!empty) {
+				s.append(", ");
+			}
+			s.append('(').append(dateOfBirth.toString()).append(')');
+		}
+		return s;
+	}
 }
