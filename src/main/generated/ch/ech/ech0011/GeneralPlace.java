485b9e773a043de9115ddaf98daec13991cad1f2
Bruno Eberhard <bruno.eberhard@pop.ch>
2019-09-09 07:37:53 +0200

    GeneralPlace implements Validation

--- src/main/generated/ch/ech/ech0011/GeneralPlace.java
+++ src/main/generated/ch/ech/ech0011/GeneralPlace.java
@@ -1,14 +1,18 @@
 package ch.ech.ech0011;
 
+import java.util.List;
+
 import org.minimalj.model.Keys;
 import org.minimalj.model.Rendering;
 import org.minimalj.model.annotation.NotEmpty;
 import org.minimalj.model.annotation.Size;
+import org.minimalj.model.validation.Validation;
+import org.minimalj.model.validation.ValidationMessage;
 import org.minimalj.repository.sql.EmptyObjects;
 import org.minimalj.util.StringUtils;
 
 // rendering
-public class GeneralPlace implements Rendering {
+public class GeneralPlace implements Rendering, Validation {
 	public static final GeneralPlace $ = Keys.of(GeneralPlace.class);
 
 	public Unknown unknown;
@@ -43,4 +47,13 @@ public class GeneralPlace implements Rendering {
 			return "Unbekannt";
 		}
 	}
+
+	@Override
+	public List<ValidationMessage> validate() {
+		if (swissTown == null && foreignCountry == null && unknown == null) {
+			return Validation.message(null, "Ungültig");
+		}
+		return null;
+	}
+
 }
